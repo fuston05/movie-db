5fa616eb2f0631ab6da0893f085d7e4544271c54
fuston05 <fuston05@yahoo.com>
2020-03-06 22:25:33 -0500

    scrapped the Grommet, I don't like using it.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //components
-import {MovieCont} from './components/MovieCont';
-import {Header} from './components/Header';
+import { MovieCont } from './components/MovieCont';
+import { Header } from './components/Header';
 
 //styles
-import {Box, Button, Heading, Grommet} from 'grommet';
-import {Notification} from 'grommet-icons';
 
-const AppBar= (props) => (
-  <Box 
-    tag= 'header'
-    direction= 'row'
-    align= 'center'
-    justify= 'between'
-    background= 'brand'
-    pad= {{ left: 'medium', right: 'small', vertical: 'small' }}
-    elevation= 'medium'
-    style= {{ zIndex: '1' }}
-    {...props}
-  />
-)//end AppBar
-
-const theme= {
-  global: {
-    colors: {
-      brand: '#228bd6'
-    },
-    font: {
-      family: 'roboto',
-      size: '18px',
-      height: '20px'
-    }//end font
-  }//end global
-}//end theme
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
   return (
-    <Grommet theme= {theme} className="App">
-      {/* <Header /> */}
-      <AppBar>
-        <Heading level= '3' margin= 'none' >My App</Heading>
-        <Button icon= {<Notification />} onClick= {() => {}}/>
-      </AppBar>
+
+    <div className= 'app'>
+      <Header />
       <MovieCont />
-    </Grommet>
+    </div>
+
   );
 }//end App
 
